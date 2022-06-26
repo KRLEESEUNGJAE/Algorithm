556f508ba78e3f34b5cd816cff978a28003373af
LEESEUNGJAE <karmaup0711@gmail.com>
2022-06-26 23:00:03 +0900

    improvement codes

--- 회문 문자열/Main.java
+++ 회문 문자열/Main.java
@@ -3,19 +3,23 @@ import java.util.*;
 public class Main {
   public String solution(String str) {
     String answer = "";
-    String reverse = "";
+    // String reverse = "";
 
     String upper = str.toUpperCase();
 
-    for (int i = upper.length() - 1; i >= 0; i--) {
-      reverse += upper.charAt(i);
-    }
+    // for (int i = upper.length() - 1; i >= 0; i--) {
+    // reverse += upper.charAt(i);
+    // }
+    String reverse = new StringBuilder(upper).reverse().toString();
 
     System.out.println("upper: " + upper + "\n" + "reverse: " + reverse);
 
-    for (int i = 0; i < upper.length(); i++) {
+    // for (int i = 0; i < upper.length(); i++) {
+    for (int i = 0; i < upper.length() / 2; i++) { // ! 시간 복잡도가 더 낮음
+      System.out
+          .println("upper.charAt(i): " + upper.charAt(i) + "\n" + "\n" + "reverse.charAt(i): " + reverse.charAt(i));
       if (upper.charAt(i) != reverse.charAt(i)) {
-        answer = "NO";
+        return "NO";
       } else {
         answer = "YES";
       }
