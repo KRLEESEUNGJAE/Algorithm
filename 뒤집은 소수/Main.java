880ae749918676db9cb75543f19108c857b1dc01
LEESEUNGJAE <karmaup0711@gmail.com>
2022-07-05 23:18:24 +0900

    0705

--- 뒤집은 소수/Main.java
+++ 뒤집은 소수/Main.java
@@ -1,8 +1,32 @@
 import java.util.*;
 
 public class Main {
-  public int[] solution(int n, int[] arr) {
-    int[] answer = arr;
+  public boolean isPrime(int n) {
+    if (n == 1) {
+      return false;
+    }
+    for (int i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public ArrayList<Integer> solution(int n, int[] arr) {
+    ArrayList<Integer> answer = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      int temp = arr[i];
+      int res = 0;
+      while (temp > 0) {
+        res = res * 10 + temp % 10;
+        temp /= 10;
+      }
+      if (isPrime(res)) {
+        answer.add((res));
+      }
+    }
 
     return answer;
   }
@@ -16,7 +40,7 @@ public class Main {
       arr[i] = kb.nextInt();
     }
     kb.close();
-    for (int x : arr) {
+    for (int x : T.solution(n, arr)) {
       System.out.print(x + " ");
     }
   }
