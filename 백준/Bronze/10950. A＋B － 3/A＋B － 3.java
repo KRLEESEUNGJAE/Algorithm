bbe8ad5cef5b9d1d19b5fa08d76d373f362f0508
Seungjae Lee <karmaup0711@gmail.com>
2022-07-15 09:22:57 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    int[] arr = new int[T];
+    
+    
+    for(int i = 0; i < T; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      arr[i] = a + b;
+    }
+
+    for(int i = 0; i < T; i++){
+      System.out.println(arr[i]);
+    }
+  }
+}
