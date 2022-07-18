8da6d52224049576f09574aeb797c9038962bd7a
LEESEUNGJAE <karmaup0711@gmail.com>
2022-07-18 21:27:53 +0900

    0718

---
+++ 자바 알고리즘 연습/임시반장 정하기/Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int N = Integer.parseInt(br.readLine());
+    int[][] arr = new int[N][N];
+    int[] answer = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+
+      }
+    }
+
+    bw.write(answer + "");
+
+    br.close();
+
+    bw.flush();
+    bw.close();
+  }
+}
