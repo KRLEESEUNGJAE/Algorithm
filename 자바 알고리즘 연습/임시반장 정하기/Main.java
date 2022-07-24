e403cac954054f8cbc23e436bba25d2b9e7a3c62
LEESEUNGJAE <karmaup0711@gmail.com>
2022-07-24 22:09:48 +0900

    0724

--- 자바 알고리즘 연습/임시반장 정하기/Main.java
+++ 자바 알고리즘 연습/임시반장 정하기/Main.java
@@ -12,22 +12,34 @@ public class Main {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int N = Integer.parseInt(br.readLine());
-    int[][] arr = new int[N][N];
-    int[] answer = new int[N];
+    int[][] arr = new int[N + 1][6];
+    int max = 0;
+    int answer = 0;
 
-    for (int i = 0; i < N; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < N; j++) {
+    StringTokenizer st;
+
+    for (int i = 1; i <= N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 1; j <= 5; j++) {
         arr[i][j] = Integer.parseInt(st.nextToken());
       }
     }
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-
+    for (int i = 1; i <= N; i++) {
+      int count = 0;
+      for (int j = 1; j <= N; j++) {
+        for (int k = 1; k <= 5; k++) {
+          if (arr[i][k] == arr[j][k]) {
+            count++;
+            break;
+          }
+        }
+      }
+      if (count > max) {
+        max = count;
+        answer = i;
       }
     }
-
     bw.write(answer + "");
 
     br.close();
