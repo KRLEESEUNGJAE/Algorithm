bbe68aa6812ae68f07b26878791486cdc9ceaeae
Seungjae Lee <karmaup0711@gmail.com>
2022-08-02 11:16:38 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    char c = br.readLine().charAt(0);
+    byte b = (byte) c;
+
+    System.out.println(b);
+  }
+}
